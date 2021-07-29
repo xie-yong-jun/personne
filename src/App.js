0539d73752ee1457eb4075f22e021ba1c31dd16a
xie-yong-jun <13253915960@163.com>
2021-07-29 15:39:12 +0800

    sass全局变量配置

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.scss';
+
 import React, { Component } from "react";
 // react-router提供了路由的核心api，包括router、route、Switch等，但是没有提供dom操作跳转
 // api react-router-dom提供BrowserRouter，route Link、switch等，可以通过dom事件控制路由
@@ -19,7 +19,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div class='text'> <h2> 谢拥军 </h2> </div>
+        <div> <h2> 谢拥军 </h2> </div>
         {/* HashRouter 路由模式，页面地址上会带有 # 号 http://localhost:3001/#/
             BrowserRouter  http://localhost:3001/
             区别 ：HashRouter最简单，不需要服务器端渲染，服务器端无论对任何URL请求都返回一模一样的HTML就好，靠浏览器的#来区分path就好
